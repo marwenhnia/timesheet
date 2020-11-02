04ba4245802da1218d922765c5b7bebc4e2dd41a
mustaphamajed <mustapha.majed@esprit.tn>
2020-11-03 00:52:21 +0100

    timesheet

--- src/test/java/tn/esprit/spring/EntrepriseServiceTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceTest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,7 +18,7 @@ import tn.esprit.spring.repository.DepartementRepository;
 
 import tn.esprit.spring.services.IEntrepriseService;
 @SpringBootTest
-@RunWith(SpringRunner.class)
+
 public class EntrepriseServiceTest {
 	@Autowired
 	IEntrepriseService es;
