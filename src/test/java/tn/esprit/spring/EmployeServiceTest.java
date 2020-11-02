bea86805b740499f124513e4c4ac6f286d694726
Marwen Hnia <marwen.hnia11@gmail.com>
2020-11-02 23:28:17 +0100

    Employe Service

--- src/test/java/tn/esprit/spring/EmployeServiceTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceTest.java
@@ -9,13 +9,13 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-import org.springframework.test.context.junit4.SpringRunner;
+
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
@@ -30,7 +30,7 @@ import tn.esprit.spring.services.IDepartementService;
 import tn.esprit.spring.services.IEmployeService;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+
 public class EmployeServiceTest {
 	
 	@Autowired
